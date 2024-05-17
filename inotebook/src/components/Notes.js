c92a48ce0f568dfcd5fdf7f752cd506c86500a6b
vika <singhvivek1010101@gmail.com>
2024-05-18 00:10:42 +0530

    alert functionality added

--- inotebook/src/components/Notes.js
+++ inotebook/src/components/Notes.js
@@ -3,7 +3,7 @@ import noteContext from "../context/notes/noteContext"
 import Noteitem from './Noteitem';
 import AddNote from './AddNote';
 
-const Notes = () => {
+const Notes = (props) => {
     const context = useContext(noteContext);
   const {notes,getNotes,editNote} = context;
   useEffect(() =>{
@@ -24,6 +24,7 @@ const Notes = () => {
   const handleClick = (e) =>{
     editNote(note.id, note.etitle, note.edescription, note.etag)
     refClose.current.click();
+    props.showAlert("Updated Successfully", "success")
     
 }
 
@@ -33,7 +34,7 @@ const onChange = (e) =>{
 
   return (
     <>
-    <AddNote/>
+    <AddNote showAlert={props.showAlert}/>
     
 <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Launch demo modal
@@ -104,7 +105,7 @@ const onChange = (e) =>{
     <div className="container mx-2">
     {notes.length===0 && "No Notes to Display"} </div>
     {notes.map((note)=>{
-      return <Noteitem key = {note._id} updateNote = {updateNote} note={note}/>;
+      return <Noteitem key = {note._id} updateNote = {updateNote} showAlert={props.showAlert} note={note}/>;
     })}
     </div>
     </>
