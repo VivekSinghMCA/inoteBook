5f9fddf471d071fe0c954814aa24e736f023c71e
vika <singhvivek1010101@gmail.com>
2024-05-18 00:11:28 +0530

    alert functionality added

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 
-const Signup = () => {
+const Signup = (props) => {
   const [credentials, setCredentials] = useState({name:"", email:"", password:"", cpassword:""})
   let navigate = useNavigate();
 
@@ -21,11 +21,10 @@ const Signup = () => {
         // save the auth token and redirect
         localStorage.setItem('token', json.authtoken)
         navigate('/')
-
-
+        props.showAlert("Account Created Successfully", "success")
       }
       else{
-        alert("Invalid Credentials");
+        props.showAlert("Invalid Credentials", "danger")
       }
 }
 const onChange = (e) =>{
@@ -37,11 +36,11 @@ const onChange = (e) =>{
       <form onSubmit={handleSubmit}>
       <div className="mb-3">
     <label htmlFor="name" className="form-label">Name</label>
-    <input type="text" className="form-control" onChange={onChange} id="name" name='name' required/>
+    <input type="text" className="form-control" onChange={onChange} id="name" name='name'/>
   </div>
   <div className="mb-3">
     <label htmlFor="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" onChange={onChange} id="email" name="email" aria-describedby="emailHelp" required/> 
+    <input type="email" className="form-control" onChange={onChange} id="email" name="email" aria-describedby="emailHelp"/> 
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
