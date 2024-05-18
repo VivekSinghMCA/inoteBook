eb77cfb1e04b220090dffcb5ed1251ef3494d20c
vika <singhvivek1010101@gmail.com>
2024-05-18 13:59:55 +0530

    changes regarding logout

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -20,7 +20,7 @@ const Signup = (props) => {
       if(json.success){
         // save the auth token and redirect
         localStorage.setItem('token', json.authtoken)
-        navigate('/')
+        navigate("/")
         props.showAlert("Account Created Successfully", "success")
       }
       else{
@@ -32,13 +32,14 @@ const onChange = (e) =>{
 }
 
   return (
-    <div className='container'>
+    <div className='container mt-2'>
+      <h2 className='my-2'>Create an account to use iNotebook</h2>
       <form onSubmit={handleSubmit}>
       <div className="mb-3">
     <label htmlFor="name" className="form-label">Name</label>
     <input type="text" className="form-control" onChange={onChange} id="name" name='name'/>
   </div>
-  <div className="mb-3">
+  <div className="my-3">
     <label htmlFor="email" className="form-label">Email address</label>
     <input type="email" className="form-control" onChange={onChange} id="email" name="email" aria-describedby="emailHelp"/> 
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
